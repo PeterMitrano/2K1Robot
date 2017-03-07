2ac4d09cb1a4db15e4a3a1167c29308065a201e7
Peter Mitrano <mitranopeter@gmail.com>
2017-03-07 04:23:15 -0500

    basic send recieve works
     - app will accept back checksums--not good
     - haven't verified correctness of data sent from app

--- robot.py
+++ robot.py
@@ -1,20 +1,32 @@
+import bluetooth
+from time import sleep, time
+
 def main():
-    import bluetooth
 
     server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
     port = 1
     server_sock.bind(("", port))
+    print("waiting for robot")
     server_sock.listen(1)
 
     client_sock, address = server_sock.accept()
     print("Accepted connection from ", address)
 
-    data = client_sock.recv(1024)
-    print("received [%s]" % data)
+    last_heartbeat_time = time()
+    while True:
+        now = time()
 
-    client_sock.close()
-    server_sock.close()
+        if now - last_heartbeat_time > 1.5:
+            last_heartbeat_time = now
+            heartbeat = bytes([0x5f, 0x05, 0x07, 0x0C, 0x00, 0xE8])
+            print("Sending: ", heartbeat)
+            client_sock.send(heartbeat)
+
+        data = client_sock.recv(1024)
+        print("Received: ", data)
+        sleep(0.1)
+
 
 
 if __name__ == '__main__':
