e8cb0123546c669ee7213d8a335efe88fcc4a1f6
Peter Mitrano <mitranopeter@gmail.com>
2017-03-07 16:05:17 -0500

    basic heartbeat example works

--- robot.py
+++ robot.py
@@ -1,32 +1,34 @@
 import bluetooth
 from time import sleep, time
 
+
 def main():
-
-    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-
     port = 1
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     server_sock.bind(("", port))
-    print("waiting for robot")
     server_sock.listen(1)
 
-    client_sock, address = server_sock.accept()
-    print("Accepted connection from ", address)
-
-    last_heartbeat_time = time()
     while True:
-        now = time()
+        print("waiting for robot")
+        client_sock, address = server_sock.accept()
+        print("Accepted connection from ", address)
 
-        if now - last_heartbeat_time > 1.5:
-            last_heartbeat_time = now
-            heartbeat = bytes([0x5f, 0x05, 0x07, 0x0C, 0x00, 0xE8])
-            print("Sending: ", heartbeat)
-            client_sock.send(heartbeat)
+        last_heartbeat_time = time()
+        while True:
+            now = time()
 
-        data = client_sock.recv(1024)
-        print("Received: ", data)
-        sleep(0.1)
+            try:
+                if now - last_heartbeat_time > 1.5:
+                    last_heartbeat_time = now
+                    heartbeat = bytes([0x5f, 0x05, 0x07, 0x0C, 0x00, 0xE7])
+                    print("Sending: ", heartbeat)
+                    client_sock.send(heartbeat)
 
+                data = client_sock.recv(1024)
+                print("Received: ", data)
+                sleep(0.1)
+            except bluetooth.btcommon.BluetoothError:
+                break
 
 
 if __name__ == '__main__':
